15d6460ed424b29963c74034ce1c076ab99b9340
Yusuke Okada <y.okada.2125@gmail.com>
2026-09-06 10:20:40 +0900

    Issue #7: categoriesテーブルのマイグレーションを実装

---
+++ database/migrations/2026_09_06_003354_create_categories_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->string('category_name', 32);
+            $table->unique(['user_id', 'category_name']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
