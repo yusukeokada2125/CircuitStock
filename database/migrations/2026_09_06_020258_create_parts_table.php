2b2f85ad7c5000a6d2d8dc697bc2e581e7649105
Yusuke Okada <y.okada.2125@gmail.com>
2026-09-08 23:54:16 +0900

    Issue #7: partsテーブルのマイグレーション実装途中

---
+++ database/migrations/2026_09_06_020258_create_parts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('parts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('category_id')->constrained()->restrictOnDelete();
+            $table->foreignId('storage_id')->constrained()->restrictOnDelete();
+            $table->string('part_name', 32);
+            $table->string('model_number', 64);
+            $table->integer('stock_quantity')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('parts');
+    }
+};
