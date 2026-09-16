3030b0d495b858a2cc2f3b1918f3a7ac2fb4e972
Yusuke Okada <y.okada.2125@gmail.com>
2026-09-16 23:41:32 +0900

    Issue #8 Userモデルをusesテーブルの定義に合わせる

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,13 +10,16 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
-#[Hidden(['password', 'remember_token'])]
+#[Fillable(['user_name', 'email', 'password'])]
+#[Hidden(['password'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
+    public $timestamps = false;
+    protected $rememberTokenName = '';
+    
     /**
      * Get the attributes that should be cast.
      *
@@ -25,7 +28,6 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
