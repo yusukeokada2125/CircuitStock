7bce2072663419eeab3623a69aecbf00c0d36e5e
Yusuke Okada <y.okada.2125@gmail.com>
2026-09-20 15:59:26 +0900

    Issue #8: 仮の部品管理画面を作成

---
+++ resources/views/parts/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>部品管理 - CircuitStock</title>
+</head>
+<body>
+    <h1>部品管理</h1>
+    <p>部品管理画面（仮）です。</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,7 @@ Route::get('/', function () {
 Route::get('/login', function () {
     return view('auth.login');
 });
+
+Route::get('/parts', function () {
+    return view('parts.index');
+});
